7fcc8d62b021727e41416052a620039fcd0b7d4a
Junjie Chen <junjie.chen18@gmail.com>
2019-04-14 15:20:52 -0400

    feat(component/SongDetail): register SongDetail with query

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
@@ -10,4 +12,4 @@ class SongDetail extends Component {
   }
 }
 
-export default SongDetail;
+export default graphql(query, { options: props => ({ variables: { id: props.params.id } }) })(SongDetail);

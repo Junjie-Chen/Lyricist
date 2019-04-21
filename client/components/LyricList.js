101eb7512281b22da7a36274e5b1655a6ee18130
Junjie Chen <junjie.chen18@gmail.com>
2019-04-21 11:45:20 -0400

    feat(component): handle optimistic response

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,8 +3,18 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
-  onLyricLike(id) {
-    this.props.mutate({ variables: { id } });
+  onLyricLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          __typename: 'LyricType',
+          id,
+          likes: likes + 1
+        }
+      }
+    });
   }
 
   renderLyrics() {
@@ -18,7 +28,7 @@ class LyricList extends Component {
           <div className="vote-box">
             <i
               className="material-icons"
-              onClick={() => this.onLyricLike(id)}
+              onClick={() => this.onLyricLike(id, likes)}
             >
               thumb_up
             </i>
